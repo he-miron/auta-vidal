28e80c5cbf90e4fb2868ad0e245692436318de30
he-miron <mironfsa@gmail.com>
2026-02-01 16:28:45 -0300

    Update CSV data source URL in app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ st.markdown("""
     """, unsafe_allow_html=True)
 
 # Link direto para os dados (Exportado como CSV)
-URL = "https://docs.google.com/spreadsheets/d/1yurzw28SK7rF6LPpbKYShICY0QgexeFbv0ShVbwUkjc/export?format=csv&gid=672132072"
+URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQV4Cj-QnWSfJLD5I5TwNfEW6F0Ti_YFPve0yyzqOAW9clUyLlRvohv9ZKm7kGD7x6xTVo0qKlYohKl/pub?output=csv"
 
 @st.cache_data(ttl=300)
 def carregar_dados():
